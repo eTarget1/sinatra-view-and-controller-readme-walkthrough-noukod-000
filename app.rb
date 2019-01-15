ce8433e1e696854c4cbb938b03d98b456049f57a
clifford <clifford@noukod.com>
2019-01-15 17:27:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
 
   get '/friends' do
     # Write your code here!
-     @friends = ['Juang Clifford LOUIS', 'Jefferson PIERRE', 'Jean CHARLES', 'Christela LOUIS', 'Jeudi JOSEPH']
+     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
   end
 end
